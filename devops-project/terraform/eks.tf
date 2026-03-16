a3e7685c072134b9e39ceb49ef05132f45cbff82
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-16 11:58:07 +0530

    Update eks.tf
    due to code changes eks module change

--- devops-project/terraform/eks.tf
+++ devops-project/terraform/eks.tf
@@ -9,26 +9,26 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # Control plane endpoint
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
   enable_cluster_creator_admin_permissions = true
 
-  # EKS Managed Node Groups
   eks_managed_node_groups = {
     main = {
-      name            = "${var.environment}-ng"
-      instance_types  = var.node_group_instance_types
+      name           = "${var.environment}-ng"
+      instance_types = var.node_group_instance_types
 
-      min_size        = var.node_group_min_size
-      max_size        = var.node_group_max_size
-      desired_size    = var.node_group_desired_size
+      min_size     = var.node_group_min_size
+      max_size     = var.node_group_max_size
+      desired_size = var.node_group_desired_size
 
       subnet_ids = module.vpc.private_subnets
 
       version  = var.eks_cluster_version
       ami_type = "AL2_x86_64"
+
+      force_update_version = true
 
       tags = {
         Name = "${var.project_name}-${var.environment}-nodes"
@@ -39,7 +39,7 @@ module "eks" {
   tags = {
     Name = local.cluster_name
   }
-}  
+}
 
 
 # Required for ALB Ingress Controller - OIDC and IAM
